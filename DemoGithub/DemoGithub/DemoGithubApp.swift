7e165dc896244b310be2950846404228aa059720
Chananchai Chanmol <chayapon1610@gmail.com>
2024-06-23 03:03:53 +0000

    frist commit

---
+++ DemoGithub/DemoGithub/DemoGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoGithubApp.swift
+//  DemoGithub
+//
+//  Created by admin on 23/6/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct DemoGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
